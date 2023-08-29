bab1d7b6437404568133b78218e2505154709fc1
Karan1110 <karancsipsvig5@gmail.com>
2023-08-29 18:37:32 +0530

    add like feature

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import AsyncStorage from "@react-native-async-storage/async-storage"
-
+import { FontAwesome5 } from "@expo/vector-icons" // Import your desired icon library
 import {
   View,
   Text,
@@ -16,7 +16,7 @@ import { useFocusEffect } from "@react-navigation/native"
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([])
-  const [quantity, setQuantity] = useState()
+  const [quantity, setQuantity] = useState("")
   const [selectedProducts, setSelectedProducts] = useState([])
 
   const handleCheckboxToggle = (productId) => {
@@ -25,6 +25,43 @@ const HomeScreen = () => {
     } else {
       setSelectedProducts([...selectedProducts, productId])
     }
+  }
+
+  const handleLike = async (productId) => {
+    const value = await AsyncStorage.getItem("token")
+    setProducts((prevProducts) => {
+      return prevProducts.map((product) => {
+        if (product._id === productId) {
+          const updatedProduct = { ...product, isLiked: !product.isLiked }
+          // Update the server data based on the new 'isLiked' value
+          const endpoint = updatedProduct.isLiked
+            ? `like/${product._id}`
+            : `dislike/${product._id}`
+
+          // Make the fetch request
+          fetch(
+            `https://freelance-api-1.onrender.com/api/invoices/${endpoint}`,
+            {
+              method: "PUT",
+              headers: {
+                "x-auth-token": value,
+              },
+            }
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              // Handle the response data if needed
+              console.log(data)
+            })
+            .catch((error) => {
+              console.error("Error updating like/dislike:", error)
+            })
+
+          return updatedProduct
+        }
+        return product
+      })
+    })
   }
 
   const handleSubmit = async () => {
@@ -41,12 +78,13 @@ const HomeScreen = () => {
         },
       }
     )
-    console.log(response.data)
+    console.log(response.data, "handle submit")
   }
 
-  useFocusEffect(() => {
+  useEffect(() => {
     fetchProducts()
-  })
+    console.log(products, "line 60")
+  }, [])
 
   const fetchProducts = async () => {
     try {
@@ -68,30 +106,42 @@ const HomeScreen = () => {
     <View style={styles.container}>
       <Text style={styles.heading}>Unapproved Product Requests</Text>
       <ScrollView>
-        {products.map((product) => (
-          <View style={styles.card} key={product._id}>
-            <Image
-              source={{
-                uri: `https://freelance-api-1.onrender.com/${product._id}`, // Replace with the correct URL
-              }}
-              style={styles.image}
-              resizeMode="cover" // Choose the appropriate resizeMode
-            />
-            <Text style={styles.cardTitle}>{product.name}</Text>
-            <Text style={{ margin: 5 }}>Price: {product.price || 99}</Text>
-            <Text style={{ margin: 5 }}>Quantity: {product.quantity || 1}</Text>
-            <Text style={{ margin: 5 }}>User: {product.user}</Text>
-            <Text style={{ margin: 5 }}>
-              Status: {product.approved ? "Approved" : "Not Yet Approved"}
-            </Text>
-            <CheckBox
-              value={selectedProducts.includes(product._id)}
-              onValueChange={() => handleCheckboxToggle(product._id)}
-            />
-          </View>
-        ))}
+        {products.map((product) => {
+          return (
+            <View style={styles.card} key={product._id}>
+              <Image
+                source={{
+                  uri: `https://freelance-api-1.onrender.com/${product._id}`, // Replace with the correct URL
+                }}
+                style={styles.image}
+                resizeMode="cover" // Choose the appropriate resizeMode
+              />
+              <Text style={styles.cardTitle}>{product.name}</Text>
+              <Text style={{ margin: 5 }}>Price: {product.price || 99}</Text>
+              <Text style={{ margin: 5 }}>
+                Quantity: {product.quantity || 1}
+              </Text>
+              <Text style={{ margin: 5 }}>User: {product.user}</Text>
+              <Text style={{ margin: 5 }}>
+                Status: {product.approved ? "Approved" : "Not Yet Approved"}
+              </Text>
+
+              <CheckBox
+                value={selectedProducts.includes(product._id)}
+                onValueChange={() => handleCheckboxToggle(product._id)}
+              />
+              <TouchableOpacity onPress={() => handleLike(product._id)}>
+                <FontAwesome5
+                  name="heart"
+                  size={24}
+                  color={product.isLiked ? "red" : "grey"}
+                />
+              </TouchableOpacity>
+            </View>
+          )
+        })}
         <TextInput
-          style={{ margin: 10, borderColor: "grey", height: 50, width: 350 }}
+          style={{}}
           placeholder="Enter the desired Quantity"
           value={quantity}
           onChangeText={setQuantity}
@@ -108,7 +158,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#fff",
+    margin: 10,
+    borderBlockColor: "grey",
+    height: 50,
+    width: 350,
+  },
+  input: {
+    borderRaidus: 4,
+    borderColor: "grey",
+    border: 3,
   },
   heading: {
     fontSize: 20,
